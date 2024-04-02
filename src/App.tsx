c4096339484374fefba075486d2f48af008ec559
Davyn Savage <dsav@udel.edu>
2024-04-02 11:49:25 -0400

    Added "Davyn Savage" to home page (starter tasks)

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p> Davyn Savage
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
