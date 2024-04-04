09d0274660ab11708fbc29e9de0fdc5f7494565b
Mayowa Ayeni <mayeni@udel.edu>
2024-04-04 16:55:41 -0400

    created a homepage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Form } from "react-bootstrap";
+import HomeScreen from "./Pages/HomeScreen";
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -26,23 +27,7 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>Stephen Sayers</p>
-        <p> Davyn Savage</p>
-        <p> Mayowa Ayeni</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeScreen></HomeScreen>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control
---
+++ src/Pages/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <p>Stephen Sayers</p>
+      <p> Davyn Savage</p>
+      <p> Mayowa Ayeni</p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
