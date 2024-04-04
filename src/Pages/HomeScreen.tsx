8bc04ea9f8bf3e868ae9a80efbb07b17a8cffc5a
Mayowa Ayeni <mayeni@udel.edu>
2024-04-04 18:05:06 -0400

    created homescreen

--- src/Pages/HomeScreen.tsx
+++ src/Pages/HomeScreen.tsx
@@ -2,21 +2,13 @@ import React from "react";
 
 export default function HomeScreen() {
   return (
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <p>Stephen Sayers</p>
-      <p> Davyn Savage</p>
-      <p> Mayowa Ayeni</p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+    <header className="container">
+      <div className="header">
+        <a href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
     </header>
   );
 }
