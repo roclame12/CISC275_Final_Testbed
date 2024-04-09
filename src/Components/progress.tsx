60ae0166d9066b1b9ddf89ee69d68e1925fa17e7
stephen <roclame12@gmail.com>
2024-04-09 14:38:45 -0400

    Made a basic version of <Quiz_progress_bar/> without formatting. Added a temp function to increase the amount of responses to see the progression of the bar

--- src/Components/progress.tsx
+++ src/Components/progress.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
-
-export function Quiz_progress_bar(responseArray: Array<string>, num_questions: number): JSX.Element {
+export function Quiz_progress_bar({responseArray, num_questions}: {
+    responseArray: Array<string>,
+    num_questions: number
+}): JSX.Element {
     /**
      * JSX element that displays a person's progress through the questions in BasicTest.tsx and DetailedTest.tsx
      *@author: Stephen Sayers
@@ -10,9 +12,12 @@ export function Quiz_progress_bar(responseArray: Array<string>, num_questions: n
      *@param: responseArray {Array<string>}: the state component containing the responses from the quizzes
      *@param: num_questions {number}: the amount of questions that the quiz contains
      */
-    const progressAmt = responseArray.length / num_questions * 100;
+    const progressPercent = Math.floor(responseArray.length / num_questions * 100);
 
     return (
-        <p>Fixme: make the progress bar</p>
+        <div>
+            <h3>Quiz Progress</h3>
+            <ProgressBar now={progressPercent} label={`${progressPercent}%`}/>
+        </div>
     );
 }
--- src/Pages/BasicTest.tsx
+++ src/Pages/BasicTest.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { useState } from 'react';
+import { Quiz_progress_bar } from "../Components/progress";
+import {Button} from "react-bootstrap";
 
 
 export default function BasicTestScreen() {
     const [responses, setResponses] = useState<string[]>([]);
+    function AddResponses(): JSX.Element {
+        return (
+            <Button onClick={ () => setResponses([...responses, "Test Response"]) }>Add Response</Button>
+        )
+    }
 
     return (
         <div>
             <h1>This is a placeholder for the basic test.</h1>
+            <Quiz_progress_bar responseArray={responses} num_questions={7}/>
+            <AddResponses/>
         </div>
     )
 }
