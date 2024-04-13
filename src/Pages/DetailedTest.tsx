3b2674c3d0de49c6331c93226fb53e5488151198
stephen <roclame12@gmail.com>
2024-04-13 14:07:58 -0400

    Added the navBar to DetailedTest.tsx

--- src/Pages/DetailedTest.tsx
+++ src/Pages/DetailedTest.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { QuizProgressBar } from "../Components/progress";
+import { NavB } from "../Components/navBar";
 import "../CSS/Tests.css";
 import Question from "../Components/Question";
 
@@ -72,16 +73,19 @@ export default function DetailedTestScreen() {
      */
 
     return (
-        <div className="test-body">
-            <QuizProgressBar answeredCount={answeredCount} num_questions={openEndedQuestions.length} />
-            <Question
-                questionText={openEndedQuestions[currentQuestionIndex].question}
-                onTextChange={handleTextChange}
-                type={openEndedQuestions[currentQuestionIndex].type}
-                textResponse={responses[currentQuestionIndex]}
-                onQuestionChange={handleQuestionChange}
-                currentQuestionIndex={currentQuestionIndex}
-            />
-        </div>
+        <>
+            <NavB/>
+            <div className="test-body">
+                <QuizProgressBar answeredCount={answeredCount} num_questions={openEndedQuestions.length} />
+                <Question
+                    questionText={openEndedQuestions[currentQuestionIndex].question}
+                    onTextChange={handleTextChange}
+                    type={openEndedQuestions[currentQuestionIndex].type}
+                    textResponse={responses[currentQuestionIndex]}
+                    onQuestionChange={handleQuestionChange}
+                    currentQuestionIndex={currentQuestionIndex}
+                />
+            </div>
+        </>
     );
 }
