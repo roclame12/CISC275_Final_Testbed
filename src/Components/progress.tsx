0817e37316f36b95c2ff50538390c1177edbfeec
stephen <roclame12@gmail.com>
2024-04-14 13:33:51 -0400

    Wrote the header and body to FinishScreen in progress.tsx and wrote accompanying CSS to style it in Progress.css
    Added buttons to FinishScreen() to either route the user to the results page or to the home screen

--- src/Components/progress.tsx
+++ src/Components/progress.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { ProgressBar, Button } from 'react-bootstrap';
 import "../CSS/Progress.css";
 import '../CSS/Tests.css'
+import {Link} from "react-router-dom";
 
 export function QuizProgressBar({answeredCount, num_questions}: {
     answeredCount: number,
@@ -28,9 +30,25 @@ export function FinishScreen(): JSX.Element {
      * @author Stephen
      *
      */
+    const navigate = useNavigate();
+
     return (
         <div className="question-container"> {/* Takes the place of the question container*/}
-            <h2>Congratulations! You've finished the test!</h2>
+            <h1>Congratulations! You've finished the test!</h1>
+            <p className="finish-text-body">
+                This marks an exciting milestone on your journey towards finding the perfect career fit. Our
+                cutting-edge AI technology is diligently analyzing your responses to provide you with
+                comprehensive insights into your strengths, preferences, and potential career paths. when you're
+                ready, click below to uncover the personalized results that await you.
+            </p>
+            <div className="finish-button-container">
+                <Button className="finish-button" onClick={() => navigate("/results")}>
+                    Take me to the results
+                </Button>
+                <Button className="finish-button">
+                    Let me review my answers
+                </Button>
+            </div>
         </div>
     )
 }
