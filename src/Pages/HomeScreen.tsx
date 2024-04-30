5911ac8f3fc2327df4b2f2a66ac2bd93db812367
stephen <roclame12@gmail.com>
2024-04-30 16:40:10 -0400

    Created files: NavBar.css - made to have a separate place for the nav bar's style
    in HomeScreen.tsx:
    Made an element <HomeScreenCard/> that will replace <BasicTest/> and <DetailedTest/>
    
    in navBar.tsx:
    Renamed file to NavBar.tsx to better fit in with the naming conventions of the other files
    
    Removed in-line styles and replaced them with the styles in NavBar.css

--- src/Pages/HomeScreen.tsx
+++ src/Pages/HomeScreen.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "../CSS/HomeScreen.css";
-import { NavB } from "../Components/navBar";
+import { NavB } from "../Components/NavBar";
 import "../Components/Footer";
 import Footer from "../Components/Footer";
 import BasicTest from "../Components/BasicTestCard";
 import DetailedTest from "../Components/DetailedTestCard";
 
+
+function HomeScreenCard({img, header, body, button_text, link}:{
+    img: string,
+    header: string,
+    body: string,
+    button_text: string,
+    link: "/basic-test" | "/detailed-test"
+}): JSX.Element {
+
+    return (
+        <div className={"home-card"} style={{backgroundSize: "contain", backgroundRepeat: "no-repeat"}}>
+            <p>text</p>
+        </div>
+    )
+}
 
 
 
@@ -14,9 +29,9 @@ export default function HomeScreen() {
   return (
     <div>
       <NavB/>
-      <div className="test-container">
+      <div className="Home-body">
                 <div className="test">
-                    <BasicTest />
+                    <HomeScreenCard img={"../Image/BasicImage.jpg"} header={"h"} body={"h"} button_text={"h"} link={"/basic-test"}/>
                 </div>
                 <div className="test">
                     <DetailedTest />
